82337c7d798d15dff64ab6a6811730b687ea433a
Zadoussi Mustapha <mustapha.zadoussi@gmail.com>
2021-03-12 20:30:32 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import { Button } from '@material-ui/core';
+
+let Jokes = 'http://api.icndb.com/jokes/random';
 
 function App() {
+
+  const [joke, setJoke] = useState("");
+
+  const show = () => {
+    axios.get(Jokes).
+      then(res => {
+        const item = res.data;
+        setJoke(item.value.joke);
+    });
+  }
+
+  useEffect(()=>{
+    show();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container bg">
+        <div className="box">
+          <h3>Chuck Norris Jokes Generator</h3>
+          <p dangerouslySetInnerHTML={{__html: joke}}></p>
+          <Button onClick = {show} variant="contained" color="primary">Get new jokes 😂</Button>
+        </div>
+      </div>
     </div>
   );
 }
